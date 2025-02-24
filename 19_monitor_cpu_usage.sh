6ab83c257e85ce56da1d19788e145eb64a2c5afe
CodeSSRockMan <41550641+CodeSSRockMan@users.noreply.github.com>
2025-02-24 12:00:20 -0300

    Update 19_monitor_cpu_usage.sh
    condicional si supera 90% avisa

--- 19_monitor_cpu_usage.sh
+++ 19_monitor_cpu_usage.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
 while true; do
-  echo "$(date '+%T') - CPU Usage: $(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8 "%"}')"
+  cpu_idle=$(top -bn1 | grep "Cpu(s)" | awk '{print $8}')
+  cpu_usage=$(echo "100 - $cpu_idle" | bc)
+
+  echo "$(date '+%T') - CPU Usage: ${cpu_usage}%"
+
+  if (( $(echo "$cpu_usage > 90" | bc -l) )); then
+    echo "ALERTA: El uso de la CPU ha superado el 90%."
+  fi
+
   sleep 5
 done
-
